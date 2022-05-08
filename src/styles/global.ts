644264654d066ba6cfda8e574920fbe1a737af47
Josanghyeon <lokijoji4@gmail.com>
2022-05-09 08:52:23 +0900

    Add : 전역 스타일 추가

---
+++ src/styles/global.ts
@@ -0,0 +1,13 @@
+import { css } from "@emotion/react";
+
+const global = css`
+  @import url("https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400&display=swap");
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: "Noto Sans KR", sans-serif;
+  }
+`;
+
+export default global;
---
+++ src/styles/theme.ts
@@ -0,0 +1,13 @@
+const theme = {
+  lightMode: {
+    white: "#ffffff",
+    black: "#000000",
+    main: "#303f9f",
+    main_light: "#666ad1",
+    main_dark: "#001970",
+    border_gray: "#ededed",
+  },
+  darkMode: {},
+} as const;
+
+export default theme;
