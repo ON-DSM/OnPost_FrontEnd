3045f9f458a3aca66efc964d26df9be5360f34e3
ipiyou <tao710803@gmail.com>
2022-06-07 16:43:02 +0900

    set: recoil 로딩중 화면설정

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,19 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
+import { Suspense } from 'react';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <BrowserRouter>
     <RecoilRoot>
-      <App />
+      <Suspense>
+        <App />
+      </Suspense>
+      
     </RecoilRoot>
   </BrowserRouter>
 );
