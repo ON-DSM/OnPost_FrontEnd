a7b06a2e8b6315432243ea128c782708fd5f9c06
ipiyou <tao710803@gmail.com>
2022-06-13 19:52:29 +0900

    set: 기능 변경,style 변경

--- src/components/ProfileSetting/modalInfo/modalChange/ModalImg.tsx
+++ src/components/ProfileSetting/modalInfo/modalChange/ModalImg.tsx
@@ -1,39 +1,41 @@
 import { ChangeEvent, useState } from 'react';
 import * as S from '../styled';
-import { DataInfoType} from '../../../../recoil/Interface'
+import { DataInfoType } from '../../../../apis/Interface';
 
-interface PropType{
-    setvalue: (value: DataInfoType) => void;
-    value: DataInfoType;
+interface PropType {
+  setvalue: (value: DataInfoType) => void;
+  value: DataInfoType;
 }
 
-export default function ModalImg({setvalue,value}:PropType){
-    const [imgBase64, setImgBase64] = useState("/images/Modal/Standard.svg");
-    const handleChangeFile = (event: ChangeEvent<HTMLInputElement>) => {
-        const reader = new FileReader();
-        reader.onloadend = () => {
-          const base64 = reader.result;
-          if (base64) setImgBase64(base64.toString());
-        };
-        if (event.target.files !== null) {
-          reader.readAsDataURL(event.target.files[0]);
-          setvalue({...value,profile:event.target.files[0]})
-        }
-      };
-      return(
-          <>
-          <S.ProfileImg src={imgBase64} />
-        <S.ImgUplordBox>
-          <input
-            id="image-input"
-            accept=".gif, .jpg, .png, .svg"
-            type="file"
-            style={{ display: 'none' }}
-            onChange={handleChangeFile}
-          />
-          <S.ImgRemove onClick={() =>setImgBase64("/images/Modal/Standard.svg")}>이미지 삭제</S.ImgRemove>
-          <S.ImgUplord htmlFor="image-input">이미지 업로드</S.ImgUplord>
-        </S.ImgUplordBox>
-          </>
-      );
+export default function ModalImg({ setvalue, value }: PropType) {
+  const [imgBase64, setImgBase64] = useState(typeof value.profile === 'string' ? value.profile : '');
+  const handleChangeFile = (event: ChangeEvent<HTMLInputElement>) => {
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      const base64 = reader.result;
+      if (base64) setImgBase64(base64.toString());
+    };
+    if (event.target.files !== null) {
+      reader.readAsDataURL(event.target.files[0]);
+      setvalue({ ...value, profile: event.target.files[0] });
+    }
+  };
+  return (
+    <>
+      <S.ProfileImg src={imgBase64} />
+      <S.ImgUplordBox>
+        <input
+          id="image-input"
+          accept=".gif, .jpg, .png, .svg"
+          type="file"
+          style={{ display: 'none' }}
+          onChange={handleChangeFile}
+        />
+        <S.ImgRemove onClick={() => setImgBase64('/images/Modal/Standard.svg')}>
+          이미지 삭제
+        </S.ImgRemove>
+        <S.ImgUplord htmlFor="image-input">이미지 업로드</S.ImgUplord>
+      </S.ImgUplordBox>
+    </>
+  );
 }
