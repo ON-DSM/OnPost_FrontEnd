067cc7f27ff256b92ff606d28971b9a5f146bfd3
ipiyou <tao710803@gmail.com>
2022-07-08 23:30:35 +0900

    ➕ :: 댓글

--- src/components/PostIn/comment/Comment.tsx
+++ src/components/PostIn/comment/Comment.tsx
@@ -1,41 +1,47 @@
 import * as S from './style';
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 import DropComment from './DropComment';
-import axios from 'axios';
-import { comment } from '../../../apis/comment/commentShow';
+import { CommentType, SubCommentType } from '../../../apis/Interface';
+import { DeleteComment } from '../../../apis/comment/deleteComment';
 
-type OpenType = {
-  id: number;
-  open: boolean;
-};
+export default function Comment({ comments }: SubCommentType) {
+  const [Open, setopen] = useState<boolean>(false);
+  const [ComArray, setArray] = useState<CommentType[]>();
 
-
-export default function Comment() {
-  const [Data,Dataset] = useState([
-      
-  ]);
-
-  const [open, setopen] = useState<OpenType>({
-    id: 0,
-    open: false,
-  });
+  const Delete = (e:FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    DeleteComment(comments.id)
+  }
   return (
-    <S.AllcommentBox>
-          <>
-            <S.UserCommentBox>
-              <S.UserImg src="/images/PostIn/ProfileImg.png" />
-              <S.CommendBox>
-                <S.UserName>소환식</S.UserName>
-                <S.UserComment>
-                  응ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ
-                </S.UserComment>
-                <S.HideBox>
-                  <S.HideTitle>댓글 달기</S.HideTitle>
+    <>
+          <S.UserCommentBox>
+            <S.UserImg src={comments.writer.profile} />
+            <S.CommendBox>
+              <S.UserName>{comments.writer.name}</S.UserName>
+              <S.UserComment>{comments.content}</S.UserComment>
+              <S.HideBox>
+                <div onClick={() => setopen(!Open)}>
+                  <S.HideTitle>
+                    {comments.moreComment
+                      ? `${comments.moreComment}개의 댓글`
+                      : '댓글 달기'}
+                  </S.HideTitle>
+
                   <S.HideImg src="/images/PostIn/minus.png" />
-                </S.HideBox>
-              </S.CommendBox>
-            </S.UserCommentBox>
-          </>
-    </S.AllcommentBox>
+                </div>
+                {comments.writer.email === sessionStorage.getItem('email') && (
+                  <form onSubmit={Delete} style={{border: '0',outline: '0'}}>
+                    <button >삭제</button>
+                  </form>
+                  
+                )}
+              </S.HideBox>
+            </S.CommendBox>
+          </S.UserCommentBox>
+      {Open && (
+        <DropComment id={comments.id} ComArray={ComArray} setArray={setArray} />
+      )}
+      <S.Line />
+    </>
   );
 }
