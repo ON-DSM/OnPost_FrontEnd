280f09af079ad3203c09b6485a88625c87adb1c2
ipiyou <tao710803@gmail.com>
2022-07-08 23:17:41 +0900

    ♻️ :: Router 스크롤 위치 고정

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,19 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import { Suspense } from 'react';
+import ScrollTop from './utils/constant/ScrollTop';
+
+
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <BrowserRouter>
     <RecoilRoot>
       <Suspense>
+        <ScrollTop />
         <App />
       </Suspense>
     </RecoilRoot>
