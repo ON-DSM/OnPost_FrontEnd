71fc8f3f885723003fb65d413c4fa44901ae516c
ipiyou <tao710803@gmail.com>
2022-07-08 23:34:22 +0900

    ♻️ :: style,퍼블리싱

--- src/components/createPost/index.tsx
+++ src/components/createPost/index.tsx
@@ -2,38 +2,55 @@ import * as S from './styled';
 import { PostRequestType } from '../../apis/Interface';
 import useForm from '../../hooks/useForm';
 import SubmitModal from './SubmitModal';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import ReturnTag from './ReturnTag';
+import { useNavigate, useParams } from 'react-router';
+import { GetterPostIn } from '../../apis/post/crud/get/PostIn';
 
 function CreatePost() {
+  const { id } = useParams();
+  const Navi = useNavigate();
   const [OpenModal, SetOpenModal] = useState<boolean>(false);
-  const [url,seturl] = useState<string>('');
-  const { Text, handleChange,SetText } = useForm<PostRequestType>({
+  const [url, seturl] = useState<string>('');
+  const { Text, handleChange, SetText } = useForm<PostRequestType>({
     title: '',
-    email:'',
+    email: '',
     profile: '',
     introduce: '',
-    context: '',
+    content: '',
     tags: '',
   });
 
-
+  useEffect(() => {
+    const Email = sessionStorage.getItem('email');
+    if (Text.email === '' && Email !== '') {
+      SetText({ ...Text, email: Email! });
+    }
+    if (id !== '0') {
+      GetterPostIn(id).then((data) => SetText(() => data.data));
+    }
+  }, []);
 
   return (
     <S.CreatePostBox>
       <S.InputText>
-        <S.TitleInput
+        <S.InputElementBox style={{padding: '70px 70px 0 70px',overflowY: 'scroll'}}>
+          <S.TitleInput
           name="title"
           placeholder="제목을 입력해주세요"
+          value={Text.title}
           onChange={handleChange}
         />
-        <ReturnTag SetText={SetText} Text={Text}/>
+        <ReturnTag SetText={SetText} Text={Text} />
         <S.ContentInput
+          name="content"
           placeholder="내용을 입력해주세요"
+          value={Text.content}
           onChange={handleChange}
         />
+        </S.InputElementBox> 
         <S.OperationBar>
-          <span style={{ marginLeft: '30px' }}>
+          <span onClick={() => Navi(-1)} style={{ marginLeft: '30px' }}>
             <img width="12" height="13.71" src="/svg/BackSpaceArrow.svg" />
             뒤로가기
           </span>
@@ -46,17 +63,18 @@ function CreatePost() {
       </S.InputText>
       <S.PreviewText>
         <h1>{Text.title}</h1>
-        <span>{Text.context}</span>
-        <img style={{width:"100%"}}/>
+        <S.Pre >{Text.content}</S.Pre>
+        <img style={{ width: '100%' }} />
       </S.PreviewText>
       {OpenModal && (
-        <SubmitModal 
+        <SubmitModal
           handleChange={handleChange}
           Text={Text}
           SetOpenModal={SetOpenModal}
           SetText={SetText}
           seturl={seturl}
           url={url}
+          id={id}
         />
       )}
     </S.CreatePostBox>
