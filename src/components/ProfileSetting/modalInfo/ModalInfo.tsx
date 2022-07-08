d7729844f7c9099ea47d90cc9b6d4c3a131e753c
ipiyou <tao710803@gmail.com>
2022-07-08 23:26:03 +0900

    ➕ :: 회원 탈퇴 병합

--- src/components/ProfileSetting/modalInfo/ModalInfo.tsx
+++ src/components/ProfileSetting/modalInfo/ModalInfo.tsx
@@ -1,53 +1,56 @@
 import { useEffect, useState } from 'react';
 import InfoChange from './modalChange/ModalChange';
 import * as S from './styled';
-import {useRecoilState} from 'recoil';
-import { ProfileState} from '../../../recoil/proflie';
+import { useRecoilState } from 'recoil';
+import { ProfileState } from '../../../recoil/proflie';
 import ModalImg from './modalChange/ModalImg';
-import DataInfo from '../../../apis/user/post/data'
+import DataInfo from '../../../apis/user/post/data';
 import { DataInfoType } from '../../../apis/Interface';
 
-
-
-export default function ModalInfo() { ///유저정보 추가
-  const [data,setdata] = useRecoilState(ProfileState)
+export default function ModalInfo() {
+  ///유저정보 추가
+  const [data, setdata] = useRecoilState(ProfileState);
   const [value, setvalue] = useState<DataInfoType>(data);
 
-  useEffect(() =>{
-    if((Object.entries(data).toString() !== Object.entries(value).toString())){
-      DataInfo(value).then(Data => setdata(Data!));
+  useEffect(() => {
+    if (Object.entries(data).toString() !== Object.entries(value).toString()) {
+      DataInfo(value).then((Data) => setdata(Data!));
     }
-  })
+  });
 
   return (
     <>
-      <S.ProfileImgBox >
-        <ModalImg setvalue={setvalue} value={value}/>
-      </S.ProfileImgBox>
-      <S.SizeName>사이즈: 200 x 200픽셀</S.SizeName>
-      <S.UserInfoBox>
-        <S.ProfileInfoBox>
-          <S.UserInfo>
-            사용자 이름
-            <S.ProfileInfoLine />
-          </S.UserInfo>
-          <S.UserInfo>
-            한줄 소개
-            <S.ProfileInfoLine />
-          </S.UserInfo>
-          <S.UserInfo>
-            이메일
-            <S.ProfileInfoLine />
-          </S.UserInfo>
-          <S.UserInfo>
-            비밀번호
-            <S.ProfileInfoLine />
-          </S.UserInfo>
-          <S.Block />
-        </S.ProfileInfoBox>
-        <InfoChange setvalue={setvalue} value={value}/>
-        <S.ProfileInfoBox></S.ProfileInfoBox>
-      </S.UserInfoBox>
+      <div>
+        <S.ProfileImgBox>
+          <ModalImg setvalue={setvalue} value={value} />
+        </S.ProfileImgBox>
+        <S.UserInfoBox>
+          <S.ProfileInfoBox>
+            <S.UserInfo>
+              이메일
+              <S.ProfileInfoLine />
+            </S.UserInfo>
+            <S.UserInfo>
+              사용자 이름
+              <S.ProfileInfoLine />
+            </S.UserInfo>
+            <S.UserInfo>
+              한줄 소개
+              <S.ProfileInfoLine />
+            </S.UserInfo>
+            <S.Block />
+            <S.Block />
+          </S.ProfileInfoBox>
+
+          <InfoChange setvalue={setvalue} value={value} />
+          
+        </S.UserInfoBox>
+      </div>
+
+      <S.ExitFormdiv >
+        <S.ExitButton>회원탈퇴</S.ExitButton>
+        <S.ExitName>탈퇴 후 모든 개인정보는 삭제됩니다.</S.ExitName>
+      </S.ExitFormdiv>
     </>
   );
 }
