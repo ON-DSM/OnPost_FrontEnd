32035294b721cef5c66d39f008067d66f2a8913a
ipiyou <tao710803@gmail.com>
2022-07-08 23:39:06 +0900

    ⛏️ :: 인터페이스

--- src/apis/Interface.ts
+++ src/apis/Interface.ts
@@ -6,9 +6,14 @@ export interface DataInfoType {
   con: boolean;
 }
 
+export interface TokenType {
+  accessToken: string;
+  refreshToken: string;
+}
+
 export interface PasswordType {
-	password:string,
-	oldpass: string,
+  newPassword: string;
+  originPassword: string;
 }
 
 export interface PostRequestType {
@@ -16,10 +21,87 @@ export interface PostRequestType {
   email: string;
   profile: File | null | string;
   introduce: string;
-  context: string;
+  content: string;
   tags: string;
 }
 
 export interface TagType {
   value: string;
 }
+
+export interface PostList{
+  id: number;
+  title: string;
+  introduce: string;
+  writer: {
+    profile: string;
+    email: string;
+    name: string;
+    introduce: string;
+  };
+  like: number;
+  tags: string;
+  profileImage: string;
+  createAt: string;
+}
+
+export interface Post {
+  id: number;
+  content: string;
+  title: string;
+  introduce: string;
+  writer: {
+    profile: string;
+    email: string;
+    name: string;
+    introduce: string;
+  };
+  doLike: boolean,
+  like: number;
+  tags: string;
+  profile: string;
+  createAt: string;
+  comments: [
+    {
+      id: number;
+      content: string;
+      writer: {
+        name: string;
+        introduce: string;
+        profile: string;
+        email: string;
+      },
+      createAt: string;
+      moreComment: number;
+    }
+  ]
+}
+
+export interface Page{
+  like: number,
+  new: number,
+}
+
+export interface MainPostType {
+  NEW: PostList[];
+  LIKE: PostList[];
+}
+
+export interface CommentType{
+    id: number;
+  content: string;
+  writer: {
+    profile: string;
+    email: string;
+    name: string;
+    introduce: string;
+  }
+  createAt: string;
+  moreComment: number;
+}
+
+export interface SubCommentType {
+  comments: CommentType
+}
+
+
