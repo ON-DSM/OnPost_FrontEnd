97b732c633323bccd0b2b60f98255438ba94f16f
ipiyou <tao710803@gmail.com>
2022-07-08 23:15:51 +0900

    ♻️ :: recoil 추가,수정

--- src/recoil/proflie.ts
+++ src/recoil/proflie.ts
@@ -4,27 +4,72 @@ import * as I from '../apis/Interface';
 export const ProfileState = atom<I.DataInfoType>({
   key: 'ProfileState',
   default: {
-    name: "",
-    introduce: "",
-    email: "",
+    name: '',
+    introduce: '',
+    email: '',
     profile: null,
     con: false,
   },
 });
 
 export const TokenClock = atom<boolean>({
-  key:"TokenKey",
+  key: 'TokenKey',
   default: false,
 });
 
 export const WritePost = atom<I.PostRequestType>({
-  key:'UserPost',
-  default:{
+  key: 'UserPost',
+  default: {
     title: '',
-    email:'',
+    email: '',
     profile: '',
     introduce: '',
-    context: '',
+    content: '',
     tags: '',
-  }
-})
+  },
+});
+
+export const LastPost = atom<I.Post>({
+  key: 'LastPost',
+  default: {
+    id: 0,
+    content: '',
+    title: '',
+    introduce: '',
+    writer: {
+      profile: '',
+      email: '',
+      name: '',
+      introduce: '',
+    },
+    doLike: false,
+    like: 0,
+    tags: '',
+    profile: '',
+    createAt: '',
+    comments: [
+      {
+        id: 0,
+        content: '',
+        writer: {
+          name: '',
+          introduce: '',
+          profile: '',
+          email: '',
+        },
+        createAt: '',
+        moreComment: 0,
+      }
+    ]
+  },
+});
+
+
+
+export const RecentToken = atom<I.TokenType>({
+  key: 'onChangeToken',
+  default: {
+    accessToken: '',
+    refreshToken: '',
+  },
+});
