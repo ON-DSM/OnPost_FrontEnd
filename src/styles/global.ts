dad8a33b17f357edae4568fa170d2a96c24604bc
ipiyou <tao710803@gmail.com>
2022-07-08 23:15:26 +0900

    ♻️ :: global style 수정

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,14 +1,21 @@
-import { css } from "@emotion/react";
+import { css } from '@emotion/react';
 
 const global = css`
-  @import url("https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400&display=swap");
+  @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400&display=swap');
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    font-family: "Noto Sans KR", sans-serif;
-    ::-webkit-scrollbar{
-      display:none;
+    font-family: 'Noto Sans KR', sans-serif;
+    overflow-x: none;
+    ::-webkit-scrollbar {
+      width: 10px;
+    }
+    ::-webkit-scrollbar-track {
+      background-color: #e4e4e4;
+    }
+    ::-webkit-scrollbar-thumb {
+      background-color: #303f9f;
     }
   }
 `;
