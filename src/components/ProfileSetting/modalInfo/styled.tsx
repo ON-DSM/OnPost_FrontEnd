863fac6e730b36a9ce8176206e766d279fd6dadd
ipiyou <tao710803@gmail.com>
2022-07-14 23:16:33 +0900

    ♻️ :: 모달 리펙토링 스타일

--- src/components/ProfileSetting/modalInfo/styled.tsx
+++ src/components/ProfileSetting/modalInfo/styled.tsx
@@ -1,50 +1,55 @@
-import styled from "@emotion/styled";
+import styled from '@emotion/styled/macro';
 ///styled 분리
 
 export const ProfileImgBox = styled.div`
   display: flex;
   flex-direction: column;
-  align-items:center;
+  align-items: center;
+`;
+
+export const ImgCamara = styled.img`
+  position: absolute;
+  display: none;
+  cursor: pointer;
+  top: 51px;
+  left: 51px;
+  width: 19.2px;
+`;
+
+export const ImgUplord = styled.label`
+  position: absolute;
+  cursor: pointer;
+  display: none;
+  width: 120px;
+  height: 120px;
+  font-size: 14px;
+  text-align: center;
+  border-radius: 100px;
+  background-color: #000000;
+  opacity: 50%;
 `;
 
 export const ProfileImg = styled.img`
   object-fit: cover;
-    width: 200px;
-    height: 200px;
-    display: block;
+  width: 120px;
+  height: 120px;
+  border-radius: 100px;
+  display: block;
+  
 `;
 
 export const ImgUplordBox = styled.div`
   display: flex;
   flex-direction: column;
-`;
-
-export const ImgRemove = styled.label`
-  width: 80px;
-  height: 25px;
-  background-color: white;
-  border: 2px solid gray;
-  font-size: 14px;
-  text-align: center;
-  margin-top: 5px;
-  border-radius: 5px;
-  :hover {
-    background-color: #e4e4e4;
+  position:absolute;
+  top: 50px;
+  width:120px;
+  height:120px;
+  &:hover ${ImgUplord} {
+    display: block;
   }
-`;
-
-export const ImgUplord = styled.label`
-  padding: 2px 12px 2px 12px;
-  margin-top:5px;
-  background-color: white;
-  border: 1px solid #777777;
-  font-size: 14px;
-  text-align: center;
-  border-radius: 5px;
-  :hover {
-    background-color: #dadaff;
-    color: #666ad1;
-    border: 1px solid #666ad1;
+  &:hover ${ImgCamara} {
+    display: block;
   }
 `;
 
@@ -68,31 +73,31 @@ export const ProfileInfoBox = styled.div`
 `;
 
 export const ExitName = styled.span`
-    font-size:14px;
-    color: #D30000;
-    margin-left:60px;
-`
+  font-size: 14px;
+  color: #d30000;
+  margin-left: 60px;
+`;
 
 export const ExitButton = styled.button`
-    font-size:12px;
-    border: 1px solid #777777;
-    border-radius:5px;
-    background-color:white;
-    padding: 1px 7px 1px 7px;
-    margin-right:30px;
-    :hover{
-        background-color: #f9d2d2;
-        border: 1px solid #D30000;
-        color: #D30000
-    }
-`
+  font-size: 12px;
+  border: 1px solid #777777;
+  border-radius: 5px;
+  background-color: white;
+  padding: 1px 7px 1px 7px;
+  margin-right: 30px;
+  :hover {
+    background-color: #f9d2d2;
+    border: 1px solid #d30000;
+    color: #d30000;
+  }
+`;
 
 export const ExitFormdiv = styled.div`
-  width:550px;
-  position:absolute;
-  z-index:-1;
+  width: 550px;
+  position: absolute;
+  z-index: -1;
   top: 78.5%;
-`
+`;
 
 export const ProfileInfo = styled.div`
   width: 300px;
@@ -109,12 +114,12 @@ export const ProfileInfoLine = styled.span`
 `;
 
 export const Profilebutton = styled.button`
-  width:70px;
-  
-  border: 1px solid ${(props) => props.color ? '#666ad1' : '#777777'};
+  width: 70px;
+
+  border: 1px solid ${(props) => (props.color ? '#666ad1' : '#777777')};
   padding: 1px 15px 1px 15px;
   border-radius: 5px;
-  color: ${(props) => props.color ? '#666ad1' : 'black'};
+  color: ${(props) => (props.color ? '#666ad1' : 'black')};
   background-color: white;
   font-size: 13px;
   :hover {
@@ -124,16 +129,16 @@ export const Profilebutton = styled.button`
 
 export const PasswordButton = styled.button`
   width: 110px;
-  border: 1px solid ${(props) => props.color ? '#666ad1' : '#777777'};
+  border: 1px solid ${(props) => (props.color ? '#666ad1' : '#777777')};
   padding: 1px 15px 1px 15px;
   border-radius: 5px;
-  color: ${(props) => props.color ? '#666ad1' : 'black'};
+  color: ${(props) => (props.color ? '#666ad1' : 'black')};
   background-color: white;
   font-size: 13px;
   :hover {
     background-color: #dadaff;
   }
-`
+`;
 
 export const Buttonin = styled.button`
   width: 40px;
@@ -143,8 +148,8 @@ export const Buttonin = styled.button`
 `;
 
 export const ProfilebuttonBox = styled.div`
-  width:120px;
-  margin-left:10px;
+  width: 120px;
+  margin-left: 10px;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
@@ -152,18 +157,73 @@ export const ProfilebuttonBox = styled.div`
   height: 100%;
 `;
 
-export const UserInfoInput = styled.input`
-  height:25px;
-  width:300px;
-  font-size: 14px;
-  border-radius: 3px;
+export const NameInfoInput = styled.input`
+  width: 100%;
+  height: 36px;
+  margin: 20px 0 20px 0;
+  padding-left: 5px;
+  font-size: 19.2px;
+  border-radius: 8px;
   border: 1px solid #666ad1;
+  :focus {
+    outline: none;
+  }
+`;
+
+export const NameInfodiv = styled.div`
+  width: 100%;
+  height: 36px;
+  margin: 25px 0 25px 0;
+  padding-left: 5px;
+  font-size: 19.2px;
+  color: #666ad1;
+  border-radius: 8px;
+`;
+
+export const IntroInfoInput = styled.textarea`
+  width: 100%;
+  height: 160px;
+  padding: 10px 0 0 8px;
+  margin: 10px 0 30px 0;
+  font-size: 12px;
+  border-radius: 8px;
+  border: 1px solid #666ad1;
+  resize: none;
+  :focus {
+    outline: none;
+  }
+`;
+
+export const IntroInfodiv = styled.div`
+  width: 100%;
+  height: 160px;
+  word-wrap: break-word; 
+  padding: 10px 4px 0 8px;
+  margin: 10px 0 30px 0;
+  font-size: 12px;
+  color: #666ad1;
+  border-radius: 8px;
+  border: 1px solid #C4C4C4;
+
+`;
+
+export const UserInfoInput = styled.input`
+  height: 24px;
+  width: 100%;
+  padding-left: 5px;
+  margin-bottom: 10px;
+  font-size: 12px;
+  border-radius: 8px;
+  border: 1px solid #666ad1;
+  :focus {
+    outline: none;
+  }
 `;
 
 export const UserInfo = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content:space-between;
+  justify-content: space-between;
   align-items: center;
   width: 100px;
   margin-right: 50px;
@@ -282,9 +342,9 @@ export const PublicProfileBallFalse = styled.div`
 `;
 
 export const Block = styled.span`
-  height:25px;
-`
+  height: 25px;
+`;
 
 export const Text = styled.span`
-  height:25px;
-`
+  height: 25px;
+`;
