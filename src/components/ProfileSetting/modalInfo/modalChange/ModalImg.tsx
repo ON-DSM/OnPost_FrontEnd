33a0df992de5a4edb618849bbe063f863ac0fc82
ipiyou <tao710803@gmail.com>
2022-07-15 08:38:07 +0900

    ⛏️ ::  hover시 디자인 변경

--- src/components/ProfileSetting/modalInfo/modalChange/ModalImg.tsx
+++ src/components/ProfileSetting/modalInfo/modalChange/ModalImg.tsx
@@ -1,14 +1,15 @@
 import { ChangeEvent, useState } from 'react';
 import * as S from '../styled';
-import { DataInfoType } from '../../../../apis/Interface';
+import { ProfileState } from '../../../../recoil/proflie';
+import { useRecoilState } from 'recoil';
+import DataInfo from '../../../../apis/user/post/data';
 
-interface PropType {
-  setvalue: (value: DataInfoType) => void;
-  value: DataInfoType;
-}
 
-export default function ModalImg({ setvalue, value }: PropType) {
-  const [imgBase64, setImgBase64] = useState(typeof value.profile === 'string' ? value.profile : '');
+export default function ModalImg() {
+  const [data,setdata] = useRecoilState(ProfileState)
+  const [imgBase64, setImgBase64] = useState(
+    typeof data.profile === 'string' ? data.profile : ''
+  );
   const handleChangeFile = (event: ChangeEvent<HTMLInputElement>) => {
     const reader = new FileReader();
     reader.onloadend = () => {
@@ -17,11 +18,12 @@ export default function ModalImg({ setvalue, value }: PropType) {
     };
     if (event.target.files !== null) {
       reader.readAsDataURL(event.target.files[0]);
-      setvalue({ ...value, profile: event.target.files[0] });
+      setdata({ ...data, profile: event.target.files[0] });
+      DataInfo(data,event.target.files[0]).then((Data) => Data && setdata(Data));
     }
   };
   return (
-    <>
+    <div>
       <S.ProfileImg src={imgBase64} />
       <S.ImgUplordBox>
         <input
@@ -31,8 +33,9 @@ export default function ModalImg({ setvalue, value }: PropType) {
           style={{ display: 'none' }}
           onChange={handleChangeFile}
         />
-        <S.ImgUplord htmlFor="image-input">이미지 업로드</S.ImgUplord>
+        <S.ImgUplord htmlFor="image-input" />
+        <S.ImgCamara src="/svg/Camara.svg" />
       </S.ImgUplordBox>
-    </>
+    </div>
   );
 }
