2139f3d4b027c15b920039a258136b11e5385ff1
ipiyou <tao710803@gmail.com>
2022-07-17 22:40:26 +0900

    ⛏️ :: 헤더 알림,검색 삭제

--- src/components/header/main/index.tsx
+++ src/components/header/main/index.tsx
@@ -1,12 +1,10 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import * as S from './style';
-import { clearStorege, getToken } from '../../../utils/token';
+import { getToken } from '../../../utils/token';
 import MyPage from '../MyPage';
 import ProfileDropDown from '../dropdown/ProfileDropdown';
 import ProfileSetting from '../../ProfileSetting/ProfileSetting';
-import Belling from '../Belling/Belling';
-import Search from '../search/SearchReuslt';
-import { useRecoilState } from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
 import { ProfileState, RecentToken } from '../../../recoil/proflie';
 import { getinfo } from '../../../apis/user/get/getinfo';
 
@@ -15,10 +13,8 @@ function Header() {
   const [Modal, SetModal] = useState<boolean>(false);
   const [DropDown, SetDropDown] = useState<boolean>(false);
   const [Set, setSet] = useState<boolean>(false);
-  const [Bell, setbell] = useState<boolean>(false);
-  const [input, setinput] = useState<boolean>(false);
-  const [remain, setmain] = useState<boolean>(false);
   const [data, newdata] = useRecoilState(ProfileState);
+  const Token = useRecoilValue(RecentToken);
 
   useEffect(() => {
     window.addEventListener('scroll', () => {
@@ -32,8 +28,7 @@ function Header() {
         newdata(data);
       })
       .catch((e) => console.log(e));
-      console.log(getToken().accessToken)
-  }, [getToken().accessToken && getToken().refreshToken]);
+  }, [Token.accessToken && Token.refreshToken]);
 
   return (
     <S.OverFlowBox>
@@ -61,7 +56,6 @@ function Header() {
           </S.HeaderNavBar>
         </S.HeaderContainer>
 
-        {remain && <Search setinput={setinput} input={input} />}
         {Modal && <MyPage SetModal={SetModal} />}
         {DropDown && (
           <ProfileDropDown
@@ -71,7 +65,6 @@ function Header() {
           />
         )}
         {Set && <ProfileSetting setSet={setSet} />}
-        {Bell && <Belling setbell={setbell} />}
       </>
     </S.OverFlowBox>
   );
