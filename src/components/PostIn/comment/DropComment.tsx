09c41cc58fcc0c91546676910a2000f274b4a1b2
ipiyou <tao710803@gmail.com>
2022-07-17 22:37:42 +0900

    ⛏️ :: 대댓글 추가삭제 연결

--- src/components/PostIn/comment/DropComment.tsx
+++ src/components/PostIn/comment/DropComment.tsx
@@ -1,28 +1,39 @@
-import { useState, useEffect, useRef, useCallback, ChangeEvent, FormEvent } from 'react';
+import { useState, useEffect, useRef, useCallback, FormEvent } from 'react';
+import { useRecoilValue } from 'recoil';
 import { ShowComment } from '../../../apis/comment/commentShow';
 import { CreateComment } from '../../../apis/comment/createComment/Comment';
 import { DeleteComment } from '../../../apis/comment/deleteComment';
 import { CommentType } from '../../../apis/Interface';
+import { ProfileState } from '../../../recoil/proflie';
 import { getToken } from '../../../utils/token';
 import * as S from './style';
 
 interface PropsType {
   comments: CommentType;
+  DeleteCom: (e: FormEvent<HTMLFormElement>, id: number) => void;
 }
 interface PropsTypetwo {
   id: number;
-  ComArray: CommentType[] | undefined;
+  ComArray: CommentType[];
   setArray: (ComArray: CommentType[]) => void;
+  setComcount: (commentCount: number) => void;
+  commentCount: number;
 }
 
-export default function DropComment({ id, ComArray, setArray }: PropsTypetwo) {
+export default function DropComment({
+  id,
+  ComArray,
+  setArray,
+  setComcount,
+  commentCount,
+}: PropsTypetwo) {
   const textareaRef = useRef<HTMLTextAreaElement | null>(null);
 
   const [value, setValue] = useState<string>('');
+  const [dum, setdum] = useState<number>(0);
+  const user = useRecoilValue(ProfileState);
   const textAreaChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-    console.log(event.target.value)
     setValue(event.target.value);
-    
   };
 
   const ShowCom = useCallback(
@@ -34,7 +45,38 @@ export default function DropComment({ id, ComArray, setArray }: PropsTypetwo) {
     [ComArray]
   );
 
-  
+  const CreateCom = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    CreateComment(true, value, id).then((data) => setdum(() => data));
+  };
+
+  useEffect(() => {
+    if (value !== '') {
+      const DEmo = {
+        id: typeof dum === 'number' ? dum : 0,
+        content: value,
+        writer: {
+          name: user.name,
+          introduce: user.introduce,
+          profile: typeof user.profile === 'string' ? user.profile : '',
+          email: user.email,
+        },
+        createAt: '',
+        moreComment: 0,
+      };
+      console.log(DEmo);
+      setArray([...ComArray, DEmo]);
+      setComcount(commentCount + 1);
+      setValue('');
+    }
+  }, [dum]);
+
+  const DeleteCom = (e: FormEvent<HTMLFormElement>, id: number) => {
+    e.preventDefault();
+    setArray(ComArray.filter((d) => d.id !== id));
+    DeleteComment(id);
+    setComcount(commentCount - 1);
+  };
 
   useEffect(() => {
     if (textareaRef && textareaRef.current) {
@@ -51,11 +93,16 @@ export default function DropComment({ id, ComArray, setArray }: PropsTypetwo) {
   return (
     <>
       <S.ReplyBox>
-        {ComArray && ComArray.map((com) => <BigComment comments={com} />)}
+        {ComArray &&
+          ComArray.map((com, i) => (
+            <BigComment key={i} DeleteCom={DeleteCom} comments={com} />
+          ))}
         {getToken().accessToken && getToken().refreshToken && (
-          <form onSubmit={() => CreateComment(true,value, String(id))} >
-            <S.ReplyInput ref={textareaRef} onChange={textAreaChange} />
-            <S.ReplyButton type={value ? "button" : 'submit'}>답글작성</S.ReplyButton>
+          <form onSubmit={CreateCom}>
+            <S.ReplyInput ref={textareaRef} value={value} onChange={textAreaChange} />
+            <S.ReplyButton type={value === '' ? 'button' : 'submit'}>
+              답글작성
+            </S.ReplyButton>
           </form>
         )}
       </S.ReplyBox>
@@ -63,25 +110,29 @@ export default function DropComment({ id, ComArray, setArray }: PropsTypetwo) {
   );
 }
 
-function BigComment({ comments }: PropsType) {
-  const Delete = (e:FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    DeleteComment(comments.id)
-  }
+function BigComment({ comments, DeleteCom }: PropsType) {
   return (
-    <S.AllcommentBox>
-      <S.UserCommentBox>
-        <S.UserImg src={comments.writer.profile} />
-        <S.CommendBox>
-          <S.UserName>{comments.writer.name}</S.UserName>
-          <S.UserComment>{comments.content}</S.UserComment>
-          {comments.writer.email === sessionStorage.getItem('email') && (
-            <form onSubmit={Delete}>
-              <button  style={{border: '0',outline: '0'}}>삭제</button>
-            </form>
-          )}
-        </S.CommendBox>
-      </S.UserCommentBox>
-    </S.AllcommentBox>
+    <>
+      <S.AllcommentBox>
+        <S.UserCommentBox>
+          <S.UserImg src={comments.writer.profile} />
+          <S.CommendBox>
+            <S.UserName>{comments.writer.name}</S.UserName>
+            <S.UserComment>{comments.content}</S.UserComment>
+            {comments.writer.email === sessionStorage.getItem('email') && (
+              <form
+                style={{ border: '0', outline: '0' }}
+                onSubmit={(e) => DeleteCom(e, comments.id)}
+              >
+                <button style={{ all: 'unset', float: 'right' }}>
+                  <img style={{cursor: 'pointer'}} src="/svg/Postin/RedTrasj.svg" />
+                </button>
+              </form>
+            )}
+          </S.CommendBox>
+        </S.UserCommentBox>
+      </S.AllcommentBox>
+      <S.Line />
+    </>
   );
 }
