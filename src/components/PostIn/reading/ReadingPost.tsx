e9e4a86f7a41edb51791339ef6134955d1fb1fbd
ipiyou <tao710803@gmail.com>
2022-07-17 22:36:03 +0900

    ⛏️ :: 댓글 대댓글 추가삭제

--- src/components/PostIn/reading/ReadingPost.tsx
+++ src/components/PostIn/reading/ReadingPost.tsx
@@ -1,14 +1,14 @@
 import * as S from './styled';
 import Comment from '../comment/Comment';
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef, FormEvent } from 'react';
 import { useNavigate, useParams } from 'react-router';
 import { GetterPostIn } from '../../../apis/post/crud/get/PostIn';
 import { PostLike } from '../../../apis/post/crud/post/like';
 import { getToken } from '../../../utils/token';
 import { DeletePost } from '../../../apis/post/crud/delete/deletePost';
 import { CreateComment } from '../../../apis/comment/createComment/Comment';
-import { useRecoilState } from 'recoil';
-import { LastPost } from '../../../recoil/proflie';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { LastPost, ProfileState } from '../../../recoil/proflie';
 import { ShowComment } from '../../../apis/comment/commentShow';
 import { DeleteComment } from '../../../apis/comment/deleteComment';
 import instance from '../../../apis/default';
@@ -19,7 +19,10 @@ export default function ReadingPost() {
   const textareaRef = useRef<HTMLTextAreaElement | null>(null);
   const [isLike, setLike] = useState<boolean>(true);
   const [value, setValue] = useState<string>('');
+  const [dum, setdum] = useState<number>(0);
   const [Post, setPost] = useRecoilState(LastPost);
+
+  const user = useRecoilValue(ProfileState);
 
   const CommentChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     setValue(event.target.value);
@@ -34,19 +37,20 @@ export default function ReadingPost() {
   }, [value]);
 
   const ClickLike = () => {
-      if (getToken().accessToken && getToken().refreshToken) {
-        const Data = {
-          email: sessionStorage.getItem('email'),
-          postId: Post.id,
-        };
-        console.log(Data);
-        instance.post(isLike ? '/post/like' : '/post/unlike', Data, {
+    if (getToken().accessToken && getToken().refreshToken) {
+      const Data = {
+        email: sessionStorage.getItem('email'),
+        postId: Post.id,
+      };
+      console.log(Data);
+      instance
+        .post(isLike ? '/post/like' : '/post/unlike', Data, {
           headers: {
             Authorization: `Bearer ${sessionStorage.getItem('accessToken')}`,
           },
-        }).then(() => setLike(!isLike));
-      }
-    ComDel(51, 1);
+        })
+        .then(() => setLike(!isLike));
+    }
   };
 
   const Delete = () => {
@@ -54,11 +58,35 @@ export default function ReadingPost() {
     Navi('/');
   };
 
-  const ComDel = (id: number, index: number) => {
-    const arr = Post.comments.filter((d, i) => index !== i);
-    ///setPost({...Post,comments: arr})
-    console.log(arr);
-    console.log(Post.comments);
+  const CreateCom = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    CreateComment(false, value, id).then((data) => setdum(() => data));
+  };
+
+  useEffect(() => {
+    if (value !== '') {
+      console.log(dum)
+      const DEmo = {
+        id: typeof dum === 'number' ? dum : 0,
+        content: value,
+        writer: {
+          name: user.name,
+          introduce: user.introduce,
+          profile: typeof user.profile === 'string' ? user.profile : '',
+          email: user.email,
+        },
+        createAt: '',
+        moreComment: 0,
+      };
+      console.log(DEmo);
+      setPost({ ...Post, comments: [...Post.comments, DEmo] });
+      setValue('');
+    }
+  }, [dum]);
+
+  const ComDel = (id: number) => {
+    const arr = Post.comments.filter((d) => d.id !== id);
+    setPost({ ...Post, comments: arr });
     DeleteComment(id);
   };
 
@@ -85,38 +113,43 @@ export default function ReadingPost() {
           {Post.tags !== null &&
             Post.tags
               .split(',')
-              .map((T, idx) => <S.PostTag key={idx}>{T}</S.PostTag>)}
+              .map((T, idx) => <S.PostTag key={idx}>{'#' + T}</S.PostTag>)}
         </S.PostTagBox>
         <S.PostImg src={Post.profile !== '' ? Post.profile : ''} />
 
         <S.PostInfo>{Post.content}</S.PostInfo>
 
         <S.PostButtonBar>
-          <S.PostButtonBox
-            onClick={ClickLike}
-            style={{ backgroundColor: !isLike ? '#86DFFF' : '' }}
-          >
-            <S.PostButtonIcon src="/images/PostIn/Heart.svg" />
-            <S.PostButtonName>
-              {!isLike ? Post.like + 1 : Post.like}
-            </S.PostButtonName>
-          </S.PostButtonBox>
+          {isLike ? (
+            <S.PostButtonBox onClick={ClickLike}>
+              <S.PostButtonIcon src="/svg/Postin/Like.svg" />
+              <S.PostButtonName>
+                {!isLike ? Post.like + 1 : Post.like}
+              </S.PostButtonName>
+            </S.PostButtonBox>
+          ) : (
+            <S.OtherButtonBox onClick={ClickLike}>
+              <S.PostButtonIcon
+                style={{ opacity: '100%' }}
+                src="/svg/Postin/Like3.svg"
+              />
+              <S.PostButtonName>
+                {!isLike ? Post.like + 1 : Post.like}
+              </S.PostButtonName>
+            </S.OtherButtonBox>
+          )}
 
           {sessionStorage.getItem('email') === Post.writer.email && (
             <S.ButtonCombine>
-              <S.PostButtonBox>
-                <S.PostButtonName
-                  onClick={() =>
-                    Navi(`/post/CreatePostPage/recent=:${Post.id}`)
-                  }
-                >
-                  수정하기
-                </S.PostButtonName>
-                <S.PostButtonIcon src="/images/PostIn/Write.png" />
+              <S.PostButtonBox
+                onClick={() => Navi(`/post/CreatePostPage/${Post.id}`)}
+              >
+                <S.PostButtonName>수정하기</S.PostButtonName>
+                <S.PostButtonIcon src="/svg/Postin/Write.svg" />
               </S.PostButtonBox>
-              <S.PostButtonBox>
-                <S.PostButtonName onClick={Delete}>삭제하기</S.PostButtonName>
-                <S.PostButtonIcon src="/images/PostIn/Trash.png" />
+              <S.PostButtonBox onClick={Delete}>
+                <S.PostButtonName>삭제하기</S.PostButtonName>
+                <S.PostButtonIcon src="/svg/Postin/TrashCan.svg" />
               </S.PostButtonBox>
             </S.ButtonCombine>
           )}
@@ -137,9 +170,7 @@ export default function ReadingPost() {
               <S.Num>{Post.comments.length}</S.Num>개의 댓글
             </S.CommentSum>
             {getToken().accessToken && getToken().refreshToken && (
-              <form
-                onSubmit={() => value !== '' && CreateComment(false, value, id)}
-              >
+              <form onSubmit={(e) => value !== '' && CreateCom(e)}>
                 <S.CommentInput
                   onChange={CommentChange}
                   ref={textareaRef}
@@ -151,7 +182,7 @@ export default function ReadingPost() {
               </form>
             )}
             {Post.comments.map((comment) => (
-              <Comment comments={comment} />
+              <Comment comments={comment} ComDel={ComDel} />
             ))}
           </S.CenterInfoBox>
         </S.CenterBox>
