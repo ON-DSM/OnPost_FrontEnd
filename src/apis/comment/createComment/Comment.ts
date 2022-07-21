baa6fb3bfb65fba575a33e70e5d200bbff94df2f
ipiyou <tao710803@gmail.com>
2022-07-21 16:13:16 +0900

    ⛏️ :: Id 비교

--- src/apis/comment/createComment/Comment.ts
+++ src/apis/comment/createComment/Comment.ts
@@ -1,18 +1,18 @@
 import instance from '../../default';
 
-export const CreateComment = (sub: boolean,content: string, id: string | undefined) => {
-  const Id = id
-  instance.post(
+export const CreateComment = (sub: boolean,content: string, id: string | number | undefined) => {
+  const Id = id ? id : 0;
+  return instance.post(
     sub ? '/comment/leave/sub' : '/comment/leave',
     {
       content: content,
       email: sessionStorage.getItem('email'),
-      parentId: parseInt(Id!),
+      parentId: typeof Id === 'string' ? parseInt(Id) :Id,
     },
     {
       headers: {
         Authorization: `Bearer ${sessionStorage.getItem('accessToken')}`,
       },
     }
-  );
+  ).then(data => data.data);
 };
