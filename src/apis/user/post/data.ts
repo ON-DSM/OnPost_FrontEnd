329c997d0f42afaabbd5161d376b8b3cf0cdf51b
ipiyou <tao710803@gmail.com>
2022-07-21 16:10:17 +0900

    ⛏️ :: Toast 추가

--- src/apis/user/post/data.ts
+++ src/apis/user/post/data.ts
@@ -1,32 +1,34 @@
 import { DataInfoType } from '../../Interface';
 import instance from '../../default';
+import { customToast } from '../../../utils/toast';
 
-
-const DataInfo = async (DataInfo: DataInfoType) => {
-  try {
-    const formData = new FormData();
-    ['name','introduce','profile','email'].map(n => {
-      if(DataInfo[n as keyof DataInfoType] !== ''){
-        if(n !== 'profile'){
-          formData.append(n,DataInfo[n as keyof DataInfoType] as Blob)
-        } else {
-            typeof DataInfo.profile !== 'string' && formData.append(n,DataInfo[n as keyof DataInfoType] as Blob)
-        }
+const DataInfo = async (DataInfo: DataInfoType, ProImg: null | File | string) => {
+  const formData = new FormData();
+  console.log(ProImg);
+  ['name', 'introduce', 'profile', 'email'].map((n) => {
+    if (DataInfo[n as keyof DataInfoType] !== '') {
+      if (n !== 'profile') {
+        formData.append(n, DataInfo[n as keyof DataInfoType] as Blob);
+      } else {
+          typeof ProImg !== 'string' &&
+            formData.append(n, ProImg as Blob);
       }
-    });
-    
-    return await instance.put<DataInfoType | null>('/member/edit', formData, {
+    }
+  });
+
+  return await instance
+    .put<DataInfoType | null>('/member/edit', formData, {
       headers: {
         'Authorization': `Bearer ${sessionStorage.getItem('accessToken')}`,
-        'Content-Type': 'multipart/form-data'
+        'Content-Type': 'multipart/form-data',
       },
-    }).then(data => data.data);
-  } catch (e) {
-    throw e;
-  }
+    })
+    .then((data) => {
+      customToast('계정 정보 변경이 완료되었습니다', 'Success');
+      console.log(data.data, '성공');
+      return data.data;
+    })
+    .catch(() => customToast('계정 정보가 너무 크거나 작습니다', 'Error'));
 };
-
-
-
 
 export default DataInfo;
